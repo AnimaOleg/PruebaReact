0d21adbc822af01146409ebf14465eac4dc26dc8
AnimaOleg <oleg.tortajada@gmail.com>
2020-11-02 13:07:00 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    // <div className="App">
+    //   <h1>Hello CodeSandbox</h1>
+    //   <h2>Start editing to see some magic happen!</h2>
+    // </div>
+
+    <div id="cuadro">
+      <h1>Hello CodeSandbox 2</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
